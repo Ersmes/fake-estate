3d0e7b589970c6b0bbd3f9728fd138cfaaaba3f9
Ersmes <esmesina02@gmail.com>
2020-07-25 11:19:55 -0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -9,7 +9,7 @@ def list_for_sale(city, state_code, limit=50):
 
     headers = {
         'x-rapidapi-host': "realtor.p.rapidapi.com",
-        'x-rapidapi-key': "6a4ed64e38mshee54b26379b40d7p107093jsn36e25acd943f"
+        # 'x-rapidapi-key': INSERT REALTOR API KEY HERE!
     }
 
     response = requests.request("GET", url,
